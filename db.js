8df0181b66bdf4c2edce8b446e722922da139063
ReinaMarlon <reinadinas.marlon@gmail.com>
2025-06-27 23:08:39 -0500

    Fixed db conn

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ import mysql from 'mysql2/promise';
 
 const db = await mysql.createConnection({
   host: process.env.DB_HOST,
-  user: "root",
-  // user: process.env.DB_USER,
+  // user: "root",
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: "cf_game",
-  // database: process.env.DB_NAME,
+  // database: "cf_game",
+  database: process.env.DB_NAME,
   port: 3306 
 });
 
