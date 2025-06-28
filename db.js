28c811d6b8e9d0c39c67c1ab92bed125ee6d1d6b
ReinaMarlon <reinadinas.marlon@gmail.com>
2025-06-28 13:49:53 -0500

    Pool DB Connection.

--- db.js
+++ db.js
@@ -15,7 +15,7 @@
 
 import mysql from 'mysql2/promise';
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
