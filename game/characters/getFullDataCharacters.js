eb9b6dfb3f5bb8eb71b9c23fdd33d84db53b0d93
ReinaMarlon <reinadinas.marlon@gmail.com>
2025-06-30 00:20:44 -0500

    SafeParse JSON

--- game/characters/getFullDataCharacters.js
+++ game/characters/getFullDataCharacters.js
@@ -2,9 +2,22 @@ import express from 'express';
 import db from '../../db.js';
 const allCharacterData = express.Router();
 
+
+function safeParseJson(input) {
+    if (typeof input === 'string') {
+        try {
+            return JSON.parse(input);
+        } catch (e) {
+            console.warn("No se pudo parsear:", input);
+            return null;
+        }
+    }
+    return input; // ya es objeto
+}
+
 allCharacterData.get('/api/characters/fulldata/', async (req, res) => {
-  try {
-    const [rows] = await db.query(`
+    try {
+        const [rows] = await db.query(`
       SELECT 
         c.id, c.name, c.health, c.attack, c.defense, c.picture, c.closePic,
         fd.fullname, fd.age, fd.description, fd.birthday,
@@ -12,32 +25,34 @@ allCharacterData.get('/api/characters/fulldata/', async (req, res) => {
       FROM characters c
       JOIN characters_fulldata fd ON fd.id_character = c.id
     `);
-//really..
-    const result = rows.map(row => ({
-      id: row.id,
-      name: row.name,
-      health: row.health,
-      attack: row.attack,
-      defense: row.defense,
-      picture: row.picture,
-      closePic: row.closePic,
+        //really..
+        const result = rows.map(row => ({
+            id: row.id,
+            name: row.name,
+            health: row.health,
+            attack: row.attack,
+            defense: row.defense,
+            picture: row.picture,
+            closePic: row.closePic,
 
-      fullname: row.fullname,
-      age: row.age,
-      description: row.description,
-      birthday: row.birthday,
-      galleryPic: row.galleryPic,
-      fullbodyPic: row.fullbodyPic,
+            fullname: row.fullname,
+            age: row.age,
+            description: row.description,
+            birthday: row.birthday,
+            galleryPic: row.galleryPic,
+            fullbodyPic: row.fullbodyPic,
+            splashArt: row.splashArt,
 
-      fragments: JSON.parse(row.fragments),
-      gunInfo: JSON.parse(row.gunInfo)
-    }));
+            fragments: safeParseJson(row.fragments),
+            gunInfo: safeParseJson(row.gunInfo)
+        }));
 
-    res.json(result);
-  } catch (err) {
-    console.error('Error al obtener personajes:', err);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
+
+        res.json(result);
+    } catch (err) {
+        console.error('Error al obtener personajes:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
 });
 
 export default allCharacterData;
