1c78a4485524a40b75e66b038a97bb3fd1c21925
ReinaMarlon <reinadinas.marlon@gmail.com>
2025-07-02 17:45:59 -0500

    Fixed routes for ExternalResources again and import path

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import http from 'http';
 import { WebSocketServer } from 'ws';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import crypto from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import dotenv from 'dotenv';
 dotenv.config();
