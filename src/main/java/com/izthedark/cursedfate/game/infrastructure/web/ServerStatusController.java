990195ac286be2bd4bb49fe9d995a752ea446dce
Marlon Estiben Reina Dinas <marlon.reina@globant.com>
2025-09-17 09:03:38 -0500

    Health status checker added.

---
+++ src/main/java/com/izthedark/cursedfate/game/infrastructure/web/ServerStatusController.java
@@ -0,0 +1,12 @@
+package com.izthedark.cursedfate.game.infrastructure.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerStatusController {
+    @GetMapping("/")
+    public String health() {
+        return "App is running!";
+    }
+}
